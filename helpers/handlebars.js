5a63c19544683a1da3f4def7d63aed347cd431e7
Nick Mitchell <nick@superlogical.org>
2018-01-26 13:22:42 +1100

    Update handlebars.js

--- helpers/handlebars.js
+++ helpers/handlebars.js
@@ -1,23 +1,28 @@
-Handlebars.registerHelper('capitalize', function(string) {
-    return string.charAt(0).toUpperCase() + string.slice(1);
-});
+let regsiterHelpers = function(Handlebars){
+    
+    Handlebars.registerHelper('capitalize', function(string) {
+        return string.charAt(0).toUpperCase() + string.slice(1);
+    });
 
-Handlebars.registerHelper('uncapitalize', function(string) {
-    return string.charAt(0).toUpperCase() + string.slice(1);
-});
+    Handlebars.registerHelper('uncapitalize', function(string) {
+        return string.charAt(0).toUpperCase() + string.slice(1);
+    });
 
-Handlebars.registerHelper('lowercase', function(string) {
-    return string.toLowerCase();
-});
+    Handlebars.registerHelper('lowercase', function(string) {
+        return string.toLowerCase();
+    });
 
-Handlebars.registerHelper('classify', function(thing) {
-    return thing.charAt(0).toUpperCase() + thing.slice(1);
-});
+    Handlebars.registerHelper('classify', function(thing) {
+        return thing.charAt(0).toUpperCase() + thing.slice(1);
+    });
 
-Handlebars.registerHelper('camel', function(thing) {
-    return thing.charAt(0).toLowerCase() + thing.slice(1);
-});
+    Handlebars.registerHelper('camel', function(thing) {
+        return thing.charAt(0).toLowerCase() + thing.slice(1);
+    });
 
-Handlebars.registerHelper('tablize', function(thing) {
-    return thing.replace(/ /g, '_');
-});
+    Handlebars.registerHelper('tablize', function(thing) {
+        return thing.replace(/ /g, '_');
+    });
+    
+}
+module.exports = registerHandlebars;
